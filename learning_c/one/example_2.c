67bf34db102b7920c645d6540d886bd3a4d754bb
Mark McSpadden <markmcspadden@gmail.com>
2011-05-29 14:00:25 -0500

    Add exercises for learning c basics

---
+++ learning_c/one/example_2.c
@@ -0,0 +1,6 @@
+main() {
+  int count;
+  puts("Please enter a number: ");
+  scanf("%d", &count);
+  printf("The number you chose was: %d\n", count);
+}
---
+++ learning_c/one/example_3.c
@@ -0,0 +1,7 @@
+main() {
+  char mcspaddens[4][8]={"Mark","Shevawn","Claire","Lauren"};
+  int idx;
+  for(idx=0;idx<4;idx++) {
+    puts(mcspaddens[idx]);
+  }
+}
