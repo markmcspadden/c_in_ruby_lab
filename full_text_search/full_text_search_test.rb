0630bd13ba0cf5f37d55ef316538be31d89f89e6
Mark McSpadden <markmcspadden@gmail.com>
2011-05-29 16:04:27 -0500

    Fix test

--- full_text_search/full_text_search_test.rb
+++ full_text_search/full_text_search_test.rb
@@ -11,17 +11,17 @@ class FullTextSearch < Test::Unit::TestCase
     @r_corpus = @corpus.collect{ |c| RubyFullTextSearchString.new(c) }
     @c_corpus = @corpus.collect{ |c| CFullTextSearchString.new(c) }
               
-    @s1_expected = [[0.46, "johnny appleseed planted many an apple trees and had an apple a day"],
-                [0.1875, "the apple doesnt fall far from the tree"]]    
+    @s1_expected = [[0.46, "Johnny Appleseed planted many an apple trees and had an apple a day."],
+                [0.1875, "The apple doesn't fall far from the tree."]]    
                 
-    @s2_expected = [[0.5, "i would like an orange tree"]] 
+    @s2_expected = [[0.5, "I would like an orange tree."]] 
     
-    @s3_expected = [[0.375, "i would like an orange tree"],
-                    [0.09375, "the apple doesnt fall far from the tree"]]        
+    @s3_expected = [[0.375, "I would like an orange tree."],
+                    [0.09375, "The apple doesn't fall far from the tree."]]        
   end  
   
   def test_ruby_to_words
-    assert_equal ["this", "is", "an", "apple"], RubyFullTextSearchString.new("This is an apple.").to_words
+    assert_equal ["this", "is", "an", "apple."], RubyFullTextSearchString.new("This is an apple.".downcase).to_words
   end
   
   def test_ruby_single_word_query    
@@ -35,12 +35,12 @@ class FullTextSearch < Test::Unit::TestCase
     assert_equal @s2_expected, result
   end
   def test_ruby_multiple_word_query                
-    result = RubyFullTextSearchString.new("orange tree").search_rank(@r_corpus)
+    result = RubyFullTextSearchString.new("orange tree.").search_rank(@r_corpus)
     assert_equal @s3_expected, result
   end
   
   def test_c_to_words
-    assert_equal ["this", "is", "an", "apple"], CFullTextSearchString.new("This is an apple.").to_words
+    assert_equal ["this", "is", "an", "apple."], CFullTextSearchString.new("This is an apple.".downcase).to_words
   end
   
   def test_c_single_word_query    
@@ -54,7 +54,7 @@ class FullTextSearch < Test::Unit::TestCase
     assert_equal @s2_expected, result
   end
   def test_c_multiple_word_query                
-    result = CFullTextSearchString.new("orange tree").search_rank(@c_corpus)
+    result = CFullTextSearchString.new("orange tree.").search_rank(@c_corpus)
     assert_equal @s3_expected, result
   end
   
